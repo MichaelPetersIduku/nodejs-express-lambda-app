fd672f1201b04912d4f0e02e27d3cc9cfde4a475
MichaelPetersIduku <michaelpetersiduuku@gmail.com>
2023-05-03 23:18:45 +0100

    connect to mongodb

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 console.log("MONGODB_URL", process.env.MONGODB_URL);
-// mongoose.connect(process.env.MONGODB_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-//   autoIndex: true,
-// });
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  // autoIndex: true,
+});
 
-// connection.on("error", (error) => {
-//   console.log(`MongoDB database connection error: ${error}`);
-//   throw error;
-// });
+connection.on("error", (error) => {
+  console.log(`MongoDB database connection error: ${error}`);
+  throw error;
+});
 
-// connection.once("open", async function () {
-//   console.log("MongoDB database connection opened successfully.");
-// });
+connection.once("open", async function () {
+  console.log("MongoDB database connection opened successfully.");
+});
 
 app.get("/", (req, res) => {
   res
