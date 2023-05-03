19ec20a54d77b62d70b2559f8b08899fa1a7d3e9
MichaelPetersIduku <michaelpetersiduuku@gmail.com>
2023-05-03 23:11:05 +0100

    debug

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const { connection } = mongoose;
+// const { connection } = mongoose;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  autoIndex: true,
-});
+console.log("MONGODB_URL", process.env.MONGODB_URL);
+// mongoose.connect(process.env.MONGODB_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   autoIndex: true,
+// });
 
-connection.on("error", (error) => {
-  console.log(`MongoDB database connection error: ${error}`);
-  throw error;
-});
+// connection.on("error", (error) => {
+//   console.log(`MongoDB database connection error: ${error}`);
+//   throw error;
+// });
 
-connection.once("open", async function () {
-  console.log("MongoDB database connection opened successfully.");
-});
+// connection.once("open", async function () {
+//   console.log("MongoDB database connection opened successfully.");
+// });
 
 app.get("/", (req, res) => {
   res
