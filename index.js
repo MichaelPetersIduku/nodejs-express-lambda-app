4d56d15cfc1f8f8fa649f170768fa28a0e3ea1e0
MichaelPetersIduku <michaelpetersiduuku@gmail.com>
2023-05-03 10:04:31 +0100

    added cors and body parser

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+app.use(bodyParser.json({ strict: false }));
 
 app.get("/", (req, res) => {
-    res.status(200).send("My test express app on aws lambda. Lets see if it works");
+  res
+    .status(200)
+    .send("My test express app on aws lambda. Lets see if it works");
 });
 
 app.get("/info", (req, res) => {
-    res.status(200).json({info: "You want an info, this is it"})
-})
+  res.status(200).json({ info: "You want an info, this is it" });
+});
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000");
-})
+  console.log("Listening on port 3000");
+});
