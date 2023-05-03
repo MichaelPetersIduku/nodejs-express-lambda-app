f7c9cf112657180e9035e39204295c1aa3b38587
MichaelPetersIduku <michaelpetersiduuku@gmail.com>
2023-05-03 16:27:28 +0100

    Added mongodb connection

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const mongoose = require("mongoose");
 
+const { connection } = mongoose;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoIndex: true,
+});
+
+connection.on("error", (error) => {
+  console.log(`MongoDB database connection error: ${error}`);
+  throw error;
+});
+
+connection.once("open", async function () {
+  console.log("MongoDB database connection opened successfully.");
+});
 
 app.get("/", (req, res) => {
   res
