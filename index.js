fb4571940279bb7f71cd1f22792a7f12f9860fd3
MichaelPetersIduku <michaelpetersiduuku@gmail.com>
2023-05-03 09:56:19 +0100

    info route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.urlencoded({extended: false}));
 
 app.get("/", (req, res) => {
     res.status(200).send("My test express app on aws lambda. Lets see if it works");
+});
+
+app.get("/info", (req, res) => {
+    res.status(200).json({info: "You want an info, this is it"})
 })
 
 app.listen(3000, () => {
